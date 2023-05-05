891b055a7aa05de3a50c842930f8dd8aec21b1a0
Marija Ilic <ilicmarija1230@gmail.com>
2023-05-05 16:42:23 +0200

    Prepravila sam ono sto mi je receno za izgled prozor MostVisited

--- TravelService/TravelService/WPF/View/MostVisited.xaml.cs
+++ TravelService/TravelService/WPF/View/MostVisited.xaml.cs
@@ -10,13 +10,17 @@ using TravelService.Repository;
 
 namespace TravelService.WPF.View
 {
-    public partial class MostVisited : Window
+    public partial class MostVisited : Window, INotifyPropertyChanged
     {
+        public int SelectedYear { get; set; }
         private readonly TourRepository _tourRepository;
         public List<Tour> MostVisitedTours { get; set; }
         public List<Tour> MostVisitedToursInYear { get; set; }
         public static List<Location> Locations { get; set; }
         public readonly LocationRepository _locationRepository;
+
+        public Guide Guide { get; set; }
+        public List<int> AvailableYears { get; set; }
 
         public MostVisited(Guide guide)
         {
@@ -24,26 +28,74 @@ namespace TravelService.WPF.View
             DataContext = this;
             _locationRepository = new LocationRepository();
             _tourRepository = new TourRepository();
+            Guide = guide;
+
+            var startYear = _tourRepository.GetAll().Min(t => t.TourStart.Year);
+            var currentYear = DateTime.Now.Year;
+            AvailableYears = Enumerable.Range(startYear, currentYear - startYear + 1).ToList();
+
+            if (AvailableYears.Count > 0)
+            {
+                SelectedYear = AvailableYears[0];
+            }
+
             List<Guest> guests = new List<Guest>();
             Locations = new List<Location>(_locationRepository.GetAll());
 
             List<Tour> guideTours = _tourRepository.FindGuidesTours(guide.Id);
             MostVisitedTours = new List<Tour> { _tourRepository.GetMostVisitedTour(guideTours, guests, Locations) };
-            MostVisitedToursInYear = new List<Tour> { _tourRepository.GetMostVisitedTour(guideTours, guests,Locations, DateTime.Now.Year) };
+            MostVisitedToursInYear = new List<Tour>();
 
-
+            UpdateMostVisitedToursInYear(guide);
+            SelectYear.SelectionChanged += YearComboBox_SelectionChanged;
 
         }
 
+
+
+        private void UpdateMostVisitedToursInYear(Guide guide)
+        {
+            if (guide == null)
+            {
+                return;
+            }
+
+            List<Guest> guests = new List<Guest>();
+            List<Tour> guideTours = _tourRepository.FindGuidesTours(guide.Id);
+
+            if (MostVisitedToursInYear == null)
+            {
+                MostVisitedToursInYear = new List<Tour>();
+            }
+
+            Tour mostVisitedTourInYear = _tourRepository.GetMostVisitedTour(guideTours, guests, Locations, SelectedYear);
+
+            if (mostVisitedTourInYear != null)
+            {
+                MostVisitedToursInYear.Clear();
+                MostVisitedToursInYear.Add(mostVisitedTourInYear);
+                OnPropertyChanged(nameof(MostVisitedToursInYear));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
+        private void YearComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedYear = (int)((ComboBox)sender).SelectedValue;
+            UpdateMostVisitedToursInYear(Guide);
+            MostVisitedToursInYear = MostVisitedTours.Where(t => t.TourStart.Year == SelectedYear).ToList();
+            OnPropertyChanged(nameof(MostVisitedToursInYear));
         }
+
+
+
+
+
+
     }
 }
