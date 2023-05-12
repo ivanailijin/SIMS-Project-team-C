ea5b253d0479f536c8dfcf13e75e76a783df587e
ivanailijin <ilijin.ra104.2020@uns.ac.rs>
2023-05-12 22:01:53 +0200

    RenovationRecommendation klasa, interfejs repozitorijum, repozitorijum i servis

---
+++ TravelService/TravelService/Application/UseCases/RenovationRecommendationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelService.Domain.Model;
+using TravelService.Domain.RepositoryInterface;
+
+namespace TravelService.Application.UseCases
+{
+    public class RenovationRecommendationService
+    {
+        private readonly IRenovationRecommendationRepository _renovationRecommendationRepository;
+        public RenovationRecommendationService(IRenovationRecommendationRepository renovationRecommendationRepository) 
+        {
+            _renovationRecommendationRepository = renovationRecommendationRepository;
+        }
+
+        public List<RenovationRecommendation> GetAll()
+        {
+            return _renovationRecommendationRepository.GetAll();
+        }
+
+        public RenovationRecommendation Save(RenovationRecommendation renovation)
+        {
+            return _renovationRecommendationRepository.Save(renovation);
+        }
+
+        public void Delete(RenovationRecommendation renovation)
+        {
+            _renovationRecommendationRepository.Delete(renovation);
+        }
+
+        public RenovationRecommendation Update(RenovationRecommendation renovation)
+        {
+            return _renovationRecommendationRepository.Update(renovation);
+        }
+    }
+}
--- TravelService/TravelService/Application/Utils/Injector.cs
+++ TravelService/TravelService/Application/Utils/Injector.cs
@@ -36,6 +36,7 @@ namespace TravelService.Application.Utils
         { typeof(ITourReservationRepository), new TourReservationRepository() },
         { typeof(IAccommodationRenovationRepository), new AccommodationRenovationRepository() },
         { typeof(ITourRequestRepository), new TourRequestRepository() },
+        { typeof(IRenovationRecommendationRepository), new RenovationRecommendationRepository() },
     };
 
         public static T CreateInstance<T>()
